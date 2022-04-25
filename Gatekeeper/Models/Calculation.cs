928960f11143528314f7ece5bfaa921b789a26c2
captainobvius <captainobvius@hotmail.com>
2022-04-25 18:23:35 -0400

    Applied input validation and formatting Added in defaults to make testing faster

--- Gatekeeper/Models/Calculation.cs
+++ Gatekeeper/Models/Calculation.cs
@@ -4,20 +4,17 @@ namespace Gatekeeper.Models
 {
     public class Calculation
     {
+        public string? FutureValueInput { get; set; } = "$1,388.23";
+        public string? PresentTerm { get; set; } = "10";
+        public string? PresentRate { get; set; } = "6.0%";
+
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
         public double FutureValue { get; set; } = 0;
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
-        public double? FutureValueInput { get; set; } = 0;
-        public int? FutureTerm { get; set; } = 0;
-        [DisplayFormat(DataFormatString = "{0:p2}")]
-        public double? FutureRate { get; set; } = 0;
+        public string? PresentValueInput { get; set; } = "$48,000.00";
+        public string? FutureTerm { get; set; } = "10";
+        public string? FutureRate { get; set; } = "3.0%";
 
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
         public double PresentValue { get; set; } = 0;
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
-        public double? PresentValueInput { get; set; } = 0;
-        public int? PresentTerm { get; set; } = 0;
-        [DisplayFormat(DataFormatString = "{0:p2}")]
-        public double? PresentRate { get; set; } = 0;
     }
 }
--- Gatekeeper/Pages/Calculation.cshtml.cs
+++ Gatekeeper/Pages/Calculation.cshtml.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.VisualBasic;
+using System.Text.RegularExpressions;
 
 namespace Gatekeeper.Pages
 {
     public class CalculationModel : PageModel
     {
+        private string pattern = @"[^\d |\.|\-]";
         public IActionResult OnGet()
         {
             return Page();
@@ -25,18 +27,34 @@ namespace Gatekeeper.Pages
 
         private double FutureValue(Calculation values)
         {
-            if (values.FutureRate != null && values.FutureTerm != null && values.PresentValueInput != null)
+            double rate;
+            double input;
+            int term;
+
+            string rateString = values.FutureRate != null ? Regex.Replace(values.FutureRate, pattern, "") : "";
+            string inputString = values.PresentValueInput != null ? Regex.Replace(values.PresentValueInput, pattern, "") : "";
+            string termString = values.FutureTerm != null ? Regex.Replace(values.FutureTerm, pattern, "") : "";
+
+            if (double.TryParse(rateString, out rate) && int.TryParse(termString, out term) && double.TryParse(inputString, out input))
             {
-                return Math.Round(Financial.FV(values.FutureRate.Value, values.FutureTerm.Value, 0, -1 * values.PresentValueInput.Value), 2);
+                return Math.Round(Financial.FV(rate / 100, term, 0, -1 * input), 2);
             }
             return 0;
         }
 
         private double PresentValue(Calculation values)
         {
-            if (values.PresentRate != null && values.PresentTerm != null && values.FutureValueInput != null)
+            double rate;
+            double input;
+            int term;
+
+            string rateString = values.PresentRate != null ? Regex.Replace(values.PresentRate, pattern, "") : "";
+            string inputString = values.FutureValueInput != null ? Regex.Replace(values.FutureValueInput, pattern, "") : "";
+            string termString = values.PresentTerm != null ? Regex.Replace(values.PresentTerm, pattern, "") : "";
+
+            if (double.TryParse(rateString, out rate) && int.TryParse(termString, out term) && double.TryParse(inputString, out input))
             {
-                return Math.Round(Financial.PV(values.PresentRate.Value, values.PresentTerm.Value, 0, -1 * values.FutureValueInput.Value), 2);
+                return Math.Round(Financial.PV(rate / 100, term, 0, -1 * input), 2);
             }
             return 0;
         }
